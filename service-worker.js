b00b04d67c4cd42a51bb804927af8d60ed8bcc5d
gangyu59 <gangyu59@hotmail.com>
2024-08-07 01:21:27 +0800

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -3,41 +3,80 @@ const urlsToCache = [
     '/',
     '/index.html',
     '/css/style.css',
+    '/manifest.json',
+    'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js',
+    'https://cdn.jsdelivr.net/npm/@tensorflow/tfjs',
+    'https://cdn.jsdelivr.net/npm/@tensorflow/tfjs-vis',
+    '/static/js/chart.js',
     '/static/js/fetchData.js',
     '/static/js/stockChart.js',
     '/static/js/generateAdvice.js',
-    '/static/js/main.js',
-    'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js'
+    '/static/js/simulateTrade.js',
+    '/static/js/transformer.js',
+    '/static/js/recommendation.js',
+    '/static/js/main.js'
 ];
 
-self.addEventListener('install', event => {
+// 安装 Service Worker
+self.addEventListener('install', function(event) {
     event.waitUntil(
         caches.open(CACHE_NAME)
-            .then(cache => {
+            .then(function(cache) {
                 console.log('Opened cache');
                 return cache.addAll(urlsToCache);
             })
     );
 });
 
-self.addEventListener('fetch', event => {
+// 捕获请求并缓存
+self.addEventListener('fetch', function(event) {
     event.respondWith(
         caches.match(event.request)
-            .then(response => {
+            .then(function(response) {
+                // Cache hit - return response
                 if (response) {
                     return response;
                 }
-                return fetch(event.request);
+
+                // IMPORTANT: Clone the request. A request is a stream and
+                // can only be consumed once. Since we are consuming this
+                // once by cache and once by the browser for fetch, we need
+                // to clone the response.
+                const fetchRequest = event.request.clone();
+
+                return fetch(fetchRequest).then(
+                    function(response) {
+                        // Check if we received a valid response
+                        if (!response || response.status !== 200 || response.type !== 'basic') {
+                            return response;
+                        }
+
+                        // IMPORTANT: Clone the response. A response is a stream
+                        // and because we want the browser to consume the response
+                        // as well as the cache consuming the response, we need
+                        // to clone it so we have two streams.
+                        const responseToCache = response.clone();
+
+                        caches.open(CACHE_NAME)
+                            .then(function(cache) {
+                                cache.put(event.request, responseToCache);
+                            });
+
+                        return response;
+                    }
+                );
             })
     );
 });
 
-self.addEventListener('activate', event => {
+// 更新 Service Worker
+self.addEventListener('activate', function(event) {
     const cacheWhitelist = [CACHE_NAME];
+
     event.waitUntil(
-        caches.keys().then(cacheNames => {
+        caches.keys().then(function(cacheNames) {
             return Promise.all(
-                cacheNames.map(cacheName => {
+                cacheNames.map(function(cacheName) {
                     if (cacheWhitelist.indexOf(cacheName) === -1) {
                         return caches.delete(cacheName);
                     }
